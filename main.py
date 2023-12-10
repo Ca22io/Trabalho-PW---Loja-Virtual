ad40aa653a996bd63d54fba8ee1ceb012926f61b
Cássio Bindaco <bindaco77@gmail.com>
2023-12-10 13:52:11 -0300

    Adicionando arquivos do master ao main

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 import uvicorn
 from routes.RootRouter import router as rootRouter
+from util.seguranca import atualizar_cookie_autenticacao
+from repositories.UsuarioRepo import UsuarioRepo
+
+UsuarioRepo.criar_tabela()
+UsuarioRepo.criar_administrador_padrao()
+UsuarioRepo.criar_usuario_padrao()
 
 app = FastAPI()
 
-app.mount(path="/static", app=StaticFiles(directory="static"),
-name="static")
+app.middleware("http")(atualizar_cookie_autenticacao)
+app.mount(path="/static", app=StaticFiles(directory="static"), name="static")
 
 app.include_router(rootRouter)
 
 if __name__ == "__main__":
- uvicorn.run(app="main:app", reload=True)
+    uvicorn.run(app="main:app", reload=True, port=8000)
